bf82c7160b5bc356eeb7c8e471469a9052a355c5
maruxD <achira15800@gmail.com>
2020-06-20 18:26:49 +0700

    Exercise4
    Exercise4 Brontodev class.

---
+++ Exercise4_Achira_M.py
@@ -0,0 +1,9 @@
+FE=70.3
+GB=64.5
+ICS=59.7
+CP=88.9
+print("---Your Score---")
+print("Foundation English:",FE)
+print("General Business:",GB)
+print("Introduction to Computer Systems:",ISC)
+print("Computer Programming:",CP)
